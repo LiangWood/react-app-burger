230597d8d3905ab799965a9d10bf429b45cfe721
梁文賓 <jack@liangwenbinde-MacBook-Pro.local>
2019-09-16 06:52:02 +0800

    2019/09/14

--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 import axios from '../../../axios-orders'
 import classes from './ContactData.module.css';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
-        }
+        orderForm: {
+            name: '',
+            email: '',
+            address: {
+                street: '',
+                postalCode: ''
+            }
+        },
+        loading: false
     }
 
     orderHandler = (e) => {
@@ -47,10 +51,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="Your Name"/>
-                <input className={classes.Input} type="email" name="email" placeholder="Your email"/>
-                <input className={classes.Input} type="text" name="street" placeholder="Street"/>
-                <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
+                <Input inputtype={'input'} type="text" name="name" placeholder="Your Name"/>
+                <Input inputtype={'input'} type="email" name="email" placeholder="Your email"/>
+                <Input inputtype={'input'} type="text" name="street" placeholder="Street"/>
+                <Input inputtype={'input'} type="text" name="postal" placeholder="Postal Code"/>
                 <Button 
                     btnType="Success"
                     clicked={this.orderHandler}
