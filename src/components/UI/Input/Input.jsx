03871bed5861eca678f6cc26872603392af48d40
Jack Liang <jack.liang.tw@pmgi.local>
2019-09-17 18:34:08 +0800

    2019/09/17

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -4,16 +4,45 @@ import classes from './Input.module.css';
 
 const Input = (props) => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
 
-    switch(props.inputtype) {
+    if(props.invalid && props.shouldValidate) {
+        inputClasses.push(classes.Invalid);
+    }
+
+    switch(props.elementType) {
         case('input'):
-            inputElement = <input className={classes.InputElement} {...props} />
+            inputElement = <input 
+            className={inputClasses.join(' ')} 
+            {...props.elementConfig} 
+            value={props.value}
+            onChange={props.changed} />
             break;
         case('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props} />
+            inputElement = <textarea 
+            className={inputClasses.join(' ')}
+            {...props.elementConfig} 
+            value={props.value}
+            onChange={props.changed} />
+            break;
+        case('select'):
+            inputElement = <select 
+            className={inputClasses.join(' ')}
+            value={props.value}
+            onChange={props.changed}>
+                {props.elementConfig.options.map(option => (
+                    <option key={option.value} value={option.value}>
+                        {option.displayValue}
+                    </option>
+                ))}
+            </select>
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props} />
+            inputElement = <input 
+            className={inputClasses.join(' ')} 
+            {...props.elementConfig} 
+            value={props.value}
+            onChange={props.changed} />
 
     }
 
--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -9,11 +9,77 @@ import classes from './ContactData.module.css';
 class ContactData extends Component {
     state = {
         orderForm: {
-            name: '',
-            email: '',
-            address: {
-                street: '',
-                postalCode: ''
+            name: {
+                elementType: 'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Your Name'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
+            },
+            street: {
+                elementType: 'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Street'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'ZIP Code'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false
+            },
+            country: {
+                elementType: 'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Country'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
+            },
+            email: {
+                elementType: 'input',
+                elementConfig:{
+                    type:'email',
+                    placeholder:'Your Email'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig:{
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: ''
             }
         },
         loading: false
@@ -22,19 +88,15 @@ class ContactData extends Component {
     orderHandler = (e) => {
         e.preventDefault()
         // console.log(this.props.ingredients);
-
         this.setState({loading: true});
+        const formData = {};
+        for(let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         const order = {
           ingredients: this.props.ingredients,
           price: this.props.price,
-          customer: {
-            name: 'Peter',
-            address: {
-              street: 'Test',
-              zipcode: '12345'
-            },
-            email: 'peter@test.com'
-          }
+          orderForm: formData
         }
         axios.post('/orders.json', order)
           .then(res => {
@@ -48,13 +110,59 @@ class ContactData extends Component {
           })
     }
 
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if(rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if(rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if(rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        return isValid;
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        // console.log(event.target.value)
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        console.log(updatedFormElement);
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm});
+    }
+
     render() {
+        const formElementArray = [];
+        for(let key in this.state.orderForm) {
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
         let form = (
-            <form>
-                <Input inputtype={'input'} type="text" name="name" placeholder="Your Name"/>
-                <Input inputtype={'input'} type="email" name="email" placeholder="Your email"/>
-                <Input inputtype={'input'} type="text" name="street" placeholder="Street"/>
-                <Input inputtype={'input'} type="text" name="postal" placeholder="Postal Code"/>
+            <form onSubmit={this.orderHandler}>
+                {formElementArray.map(formElement => (
+                    <Input
+                        key={formElement.id} 
+                        elementType={formElement.config.elementType} 
+                        elementConfig={formElement.config.elementConfig} 
+                        defaultvalue={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        changed={(event) => {this.inputChangedHandler(event, formElement.id)}} />
+                ))}
                 <Button 
                     btnType="Success"
                     clicked={this.orderHandler}
